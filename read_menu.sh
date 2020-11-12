d48df8d25651d6f0f6296335d82c9efe2800dcea
Lei Wang <1357492993@qq.com>
2020-11-12 10:41:16 +0800

    initial project version

---
+++ read_menu.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# read_menu.sh: a menu driven system information program
+
+clear
+echo "
+please Select:
+
+1. Display System Information
+2. Display Disk Space
+3. Display Home Space Utilization
+0. Quit
+"
+
+read -p "Enter selection [0-3] > "
+
+if [[ $REPLY =~ ^[0-3]$ ]]; then
+	if [[ $REPLY == 0 ]]; then
+		echo "Program terminated."
+		exit
+	elif [[ $REPLY == 1 ]]; then
+		echo "Hostname: $HOSTNAME"
+		uptime
+		exit
+	elif [[ $REPLY == 2 ]]; then
+		df -h
+		exit
+	elif [[ $REPLY == 3 ]]; then
+		if [[ $(id -u) -eq 0 ]]; then
+			echo "Home Space Utilization (All Users)"
+			du -sh /home/*
+		else
+			echo "Home Space Utilization ($USER)"
+			du -sh $HOME
+		fi
+		exit
+	fi
+else
+	echo "Invalid entry." >&2
+	exit 1
+fi
