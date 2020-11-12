854f0f19a69572bcf795834e9454054311ac29dc
Lei Wang <1357492993@qq.com>
2020-11-12 10:44:46 +0800

    test

--- while_read.sh
+++ while_read.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# while_read.sh: read lines from a file
+# while_read.sh: read lines from a file.
 
 while read distro version release; do
 	printf "Distro: %s\tVersion: %s\tReleased: %s\n" \
